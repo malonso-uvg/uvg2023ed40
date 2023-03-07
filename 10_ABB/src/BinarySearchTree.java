ecaa026c2872358fc0b3df1c1e8c141ffb9d971c
Moises Alonso <maalonso@uvg.edu.gt>
2023-03-07 10:17:08 -0600

    Preorder y search implementados

--- 10_ABB/src/BinarySearchTree.java
+++ 10_ABB/src/BinarySearchTree.java
@@ -56,6 +56,33 @@ public class BinarySearchTree<K, V> {
 		}
 	}
 	
+	public V search(K key) {
+		return (V) internalSearch(root, key);
+	}
+	
+	private V internalSearch(TreeNode<K, V> actual, K key) {
+		
+		if (actual != null) {
+			
+			int result = keyComparator.compare(actual.getKey(), key);
+			
+			// SI result es 0 entonces son iguales
+			if (result == 0) {
+				return actual.getValue();
+			} else if (result > 0) {
+				return (V) internalSearch(actual.getLeft(), key);
+			}else {
+				return (V) internalSearch(actual.getRight(), key);
+			}
+			
+			
+		} else {
+			return null;
+		}
+		
+		
+	}
+	
 	public void InOrderTraversal(ITraversal<K, V> visitador) {
 		internalInOrder(root, visitador);
 	}
@@ -88,14 +115,37 @@ public class BinarySearchTree<K, V> {
 		if (actual != null) {
 			
 			if (actual.getLeft() != null) {
-				internalInOrder(actual.getLeft(), visitador);
+				internalPostOrder(actual.getLeft(), visitador);
 			}
 			
 			if (actual.getRight() != null) {
-				internalInOrder(actual.getRight(), visitador);
+				internalPostOrder(actual.getRight(), visitador);
 			}
 			
 			visitador.visit(actual);
+			
+		}
+		
+	}
+	
+	
+	public void PreOrderTraversal(ITraversal<K, V> visitador) {
+		internalPreOrder(root, visitador);
+	}
+	
+	private void internalPreOrder(TreeNode<K, V> actual, ITraversal<K, V> visitador) {
+		
+		if (actual != null) {
+			
+			visitador.visit(actual);
+			
+			if (actual.getLeft() != null) {
+				internalPreOrder(actual.getLeft(), visitador);
+			}
+			
+			if (actual.getRight() != null) {
+				internalPreOrder(actual.getRight(), visitador);
+			}
 			
 		}
 		
